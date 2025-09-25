3ccf2aeefdd405cc7d0b6557781c5ffc09282c58
Aditya-kori <16adityakori@gmail.com>
2025-09-25 10:56:20 +0530

    asssertion_implemeted

--- top.sv
+++ top.sv
@@ -3,6 +3,7 @@ import uvm_pkg::*;
 `include "mem_common.sv"
 `include "mem_tx.sv"
 `include "memory.v"
+`include "mem_assert.sv"
 `include "mem_intrf.sv"
 `include "mem_driver.sv"
 `include "mem_monitor.sv"
@@ -26,6 +27,17 @@ memory dut(
 			.valid(pif.valid),
 			.ready(pif.ready)
 			);
+
+mem_assert uut(
+				.clk(pif.clk), 
+				.rst(pif.rst), 
+				.wr_rd(pif.wr_rd), 
+				.addr(pif.addr),
+				.wdata(pif.wdata), 
+				.rdata(pif.rdata), 
+				.valid(pif.valid), 
+				.ready(pif.ready)
+				);
 	always #5 clk=~clk;
 	initial begin
 		uvm_config_db#(virtual mem_intrf)::set(null,"*","vif",pif);
